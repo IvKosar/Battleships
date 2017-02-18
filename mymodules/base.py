e4897deacf080b11d89789e579a1f8350f33f9c4
IvKosar <kosarevych@ucu.edu.ua>
2017-02-18 19:55:16 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	mymodules/field_generator.py
    all classes ready

--- mymodules/base.py
+++ mymodules/base.py
@@ -1,17 +1,22 @@
 """
-
+# File: base.py
+# Contains classes for game
+# Created by Ivan Kosarevych
+# 18.02.17 19:00:17
 """
 from field_generator import *
 
 class Ship(object):
     """
-
+    Information about ship, including length, coordinates, orientation, hit decks
     """
     def __init__(self, length, bow, horizontal = True):
         """
-        :param length:
-        :param bow:
-        :param horizontal:
+        :param length: tuple, example for horizontal 2Xship: (1,2)
+        :param bow: tuple(int,int), coordinates of left upper deck
+        :param horizontal: bool
+
+        Initializes new ship
         """
         self._length = length
         self.bow = bow
@@ -22,6 +27,13 @@ class Ship(object):
 
 
     def shoot_at(self, point):
+        """
+        :param point: tuple
+        :return: None
+
+        Shows hitting a deck of a ship
+
+        """
         if self.horizontal:
             deck = point[1] - self.bow[1]
         else:
@@ -31,11 +43,20 @@ class Ship(object):
 
 
 class Field:
+    """
+    Information about field, its appearance(ships, hit ships, misses),
+    random field generation
+    """
     def __init__(self):
+        """
+        Initializes random field
+        """
+        # create empty field
         self._ships = [[None for i in range(10)] for j in range(10)]
         ships_size = [4, 3, 2, 1]
         field = []
-
+        
+        # fill it with ships
         for count in range(len(ships_size)):
             # put as many ships of each type as required
             for i in range(count + 1):
@@ -49,7 +70,8 @@ class Field:
                         shp = generate_ship(ships_size[count])
                         horizontal, ship_coordiantes = shp[0], shp[1]
                         ship_area = make_shp_area(ship_coordiantes)
-
+                
+                # add ship object to field
                 field.extend(ship_coordiantes)
                 length = (1, ships_size[count]) if horizontal else (ships_size[count], 1)
                 ship_object = Ship(length, ship_coordiantes[0], horizontal = horizontal)
@@ -59,17 +81,18 @@ class Field:
 
     def shoot_at(self, point):
         """
-
-        :param point:
-        :return:
+        :param point: tuple(int, int)
+        :return: object or None
+        
+        Demonstrates shoots on field: point on field becomes True if any ship was hit
+        or False otherwise
         """
         row,column = point
         if self._ships[row][column] and self._ships[row][column] is not True:
-            #print(self._ships[row][column])
             self._ships[row][column].shoot_at(point)
             ship = self._ships[row][column]
             self._ships[row][column] = True
-            print('ship_obj = ', ship)
+
             return ship
 
         self._ships[row][column] = False
@@ -78,8 +101,9 @@ class Field:
 
     def field_with_ships(self):
         """
-
-        :return:
+        :return: str
+        
+        Makes string with player's ships and opponent's shoots
         """
         field_str = ' '
         for i in range(1, 10):
@@ -95,7 +119,7 @@ class Field:
                 if self._ships[row][column] is True:
                     field_str += ' X '
                 elif self._ships[row][column] is False:
-                    field_str += ' '+ u"\u2022" + ' '              # bullet
+                    field_str += ' '+ u"\u2022" + ' '     # bullet
                 elif self._ships[row][column]:
                     field_str += ' * '
                 else:
@@ -112,8 +136,9 @@ class Field:
 
     def field_without_ships(self):
         """
-
         :return: str
+        
+        Makes string with player's shoots on opponent's field 
         """
         field = Field.field_with_ships(self)
         field = field.replace('*', ' ')
@@ -123,12 +148,14 @@ class Field:
 
 class Player:
     """
-
+    Info about player, including name and sunk ships. Interaction with
+    user, including getting coordinates to shoot
     """
     def __init__(self, name):
         """
+        :param name: str
 
-        :param name:
+        Initializes new player
         """
         self._name = name
         self.sunk_ships = 0
@@ -136,34 +163,37 @@ class Player:
 
     def read_position(self):
         """
-
-        :return:
+        :return: tuple(int,int)
+        
+        Reads game coordinates of shoot and converts them to coordinates in list
+        of field
         """
         from string import ascii_uppercase
 
         battleship_positions = [let + str(i) for let in ascii_uppercase[:10] for i in range(1, 10 + 1)]
         list_positions = [(row, column) for row in range(10) for column in range(10)]
-        convert_table = dict(zip(battleship_positions, list_positions))
+        compliance_table = dict(zip(battleship_positions, list_positions))
 
+        # get coordinates from user until they are correct
         battleship_position  = input(self._name + ', enter position to shoot at: ').strip()
         while battleship_position not in battleship_positions:
             battleship_position = input('There is no position you entered, try again: ').strip()
 
-        return convert_table[battleship_position]
+        return compliance_table[battleship_position]
 
 
 class Game:
     """
-
+    Combines Fields and Players
     """
     def __init__(self):
         """
-
+        Initializes new game
         """
-        import os
+        import os, time
 
         player1_name = input('Player 1, enter your name: ')
-        os.system('cls')############################################### TO SOLVE
+        os.system('cls')
         player2_name = input('Player 2, enter your name: ')
 
         self._players = [Player(player1_name), Player(player2_name)]
@@ -173,18 +203,28 @@ class Game:
     def shoot_at(self, player_index, field_index):
         """
 
-        :param point:
-        :return:
+        :param player_index: int
+        :param: field_index: int
+        :return: ship object if ship was hit, None otherwise
+
+        Makes shoot on given field, raises quantity of sunk ships in given player,
+        if ship was sunk
         """
+        # Read position to shoot
         point = self._players[player_index].read_position()
+        # make shoot on field
         ship = self._field[field_index].shoot_at(point)
-        print(ship)
+
+        # check whether we hit a ship and if so whether ship is wounded or killed
+        # and show it
         try:
             ship_len = ship._length[1] if ship.horizontal else ship._length[0]
             if ship._hit == [True for i in range(ship_len)]:
                 self._players[player_index].sunk_ships += 1
+                print('\nKILLED!')
+            else:
+                print('\nWOUNDED!')
         except:
-            print('exception')
             return
 
         return ship
@@ -192,9 +232,10 @@ class Game:
 
     def field_with_ships(self, index):
         """
+        :param index: int
+        :return: None
 
-        :param index:
-        :return:
+        Show player's field with opponent's shoots
         """
         print(self._field[index].field_with_ships())
 
@@ -202,23 +243,17 @@ class Game:
 
     def field_without_ships(self, index):
         """
+        :param index: int
+        :return: None
 
-        :param index:
-        :return:
+        Show player's shoots on opponent's field
         """
         print(self._field[index].field_without_ships())
 
 
     def check_winner(self, player_index):
         """
-
-        :param player_index:
-        :return:
+        :param player_index: int
+        :return: int or None
         """
         if self._players[player_index].sunk_ships == 10: return player_index
-
-
-#game = Game()
-#game.field_without_ships(0)
-#game.field_without_ships(1)
-#print(game.shoot_at(0,1))
--- mymodules/run.py
+++ mymodules/run.py
@@ -1,16 +1,20 @@
 """
-
+# File: run.py
+# Organizes user interface, runs the game
+# Created by Ivan Kosarevych
+# 18.02.2017 19:06:57
 """
 
-from game import Game
-import os, time
+from base import Game
+import time, os
 
 
 game = Game()
 while True:
-    if game.check_winner(game._current_player):
-        winner = game._players[game._current_player % 2]._name
-        print(winner, 'WON!')
+    os.system('cls')
+
+    if game.check_winner((game._current_player + 1) % 2):
+        winner = game._players[(game._current_player + 1) % 2]._name
         break
 
     print('\n')
@@ -18,12 +22,10 @@ while True:
     print('\n')
     game.field_without_ships((game._current_player - 1) % 2)
 
-    #print(game.shoot_at(game._current_player, (game._current_player + 1) % 2))
     if not game.shoot_at(game._current_player, (game._current_player + 1) % 2):
-        print("Please give a computer to your opponent")
-        time.sleep(1)
+        input("Press any key and give a computer to your opponent")
         game._current_player = (game._current_player + 1) % 2
-    #else:
-    #   game.shoot_at(game._current_player, (game._current_player + 1) % 2)
 
+    input('Press any key to start')
 
+print(winner, 'WON!')
